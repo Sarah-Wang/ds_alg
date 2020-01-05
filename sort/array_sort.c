4afb38da3b3e9be4294d5a91d7571aa79dc47e92
Sarah_Wang <yyzxml@outlook.com>
2020-01-05 23:20:41 +0800

    feat: add bubble sort samples
    Signed-off-by: Sarah_Wang <yyzxml@outlook.com>

--- sort/array_sort.c
+++ sort/array_sort.c
@@ -67,7 +67,68 @@ int sort_insert(int *a, int size)
 	return 0;
 }
 
-static int init_array(int *a, int size)
+//1、时间复杂度：O(n2)
+//2、空间复杂度：O(1)
+//3、稳定排序
+//4、原地排序
+int sort_bubble(int *a, int size)
+{
+	int idx, i, j, k;
+	int tmp = 0;
+
+	if (!a)
+		return -1;
+	if (size == 1)
+		return 0;
+
+	for (i = 0; i < size - 1; i++) {
+		for (j = 0; j < size - 1 - i; j++) {
+			if (a[j] > a[j + 1]) {
+				tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+			}
+		}
+	}
+	printf("%s after sort:\n", __func__);
+	for (i = 0; i < size; i++)
+		printf(" %d ", a[i]);
+	printf("\n\n");
+	return 0;
+}
+
+int sort_bubble_optimize(int *a, int size)
+{
+	int idx, i, j, k;
+	int tmp, swap;
+
+	if (!a)
+		return -1;
+	if (size == 1)
+		return 0;
+
+	for (i = 0; i < size - 1; i++) {
+		swap = 0;
+		for (j = 0; j < size - 1 - i; j++) {
+			if (a[j] > a[j + 1]) {
+				tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+				swap = 1;
+			}
+		}
+		if (swap == 0)
+			break;
+	}
+	printf("%s after sort:\n", __func__);
+	for (i = 0; i < size; i++)
+		printf(" %d ", a[i]);
+	printf("\n\n");
+	return 0;
+}
+
+
+static void init_array(int *a, int size)
 {
 	int i;
 
@@ -81,7 +142,7 @@ static int init_array(int *a, int size)
 	printf("\n\n");
 }
 
-void main(void)
+int main(void)
 {
 	int a[ARRAY_LENGTH] = {0};
 	int i;
@@ -90,4 +151,9 @@ void main(void)
 
 	sort_select(a, ARRAY_LENGTH);
 	sort_insert(a, ARRAY_LENGTH);
+	sort_insert(a, ARRAY_LENGTH);
+	sort_bubble(a, ARRAY_LENGTH);
+	sort_bubble_optimize(a, ARRAY_LENGTH);
+
+	return 0;
 }
